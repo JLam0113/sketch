625d4c2ca18feff3d632c09992779e7a8ca75e09
Jessye Lam <jessyel13@gmail.com>
2024-01-20 00:08:21 -0500

    added click and max size

--- sketch.js
+++ sketch.js
@@ -2,17 +2,20 @@ function createGrid(size){
     let ratio = 1/size;
     const container = document.querySelector('.container');
     container.innerHTML = '';
-    const grid = '<div class="grid" style="flex-basis:' + ratio*100 +'%"></div>';
+    const grid = '<div class="grid" style="flex-basis:' + ratio*100 +'%" onclick="change(this)"></div>';
     for(let i = 0; i < (size*size); i++){
         container.innerHTML += grid;
     }
 }
 
 function userPrompt(){
-    let size = prompt("Enter your grid size");
+    let size = prompt("Enter your grid size (Max 100)");
     if (isNumeric(size)){
+        if(size < 101)
         createGrid(Math.round(size));
+        else userPrompt();
     }
+    
 }
 
 function isNumeric(str) {
@@ -20,5 +23,9 @@ function isNumeric(str) {
     return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
            !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
   }
+
+function change(square){
+    square.style.background = "purple";
+}
 
 createGrid(16);
